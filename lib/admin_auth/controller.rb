fdaaf2c121cb2de09ba7d41aa4088f543cc19c8e
Phillip Boksz <phillip@lunarlogic.io>
2015-02-12 13:18:55 +0100

    Add controller module

---
+++ lib/admin_auth/controller.rb
@@ -0,0 +1,34 @@
+module AdminAuth
+  module Controller
+    def self.included(controller)
+      controller.helper_method :current_admin
+      controller.helper_method :locale
+    end
+
+    def authenticate_admin!
+      redirect_to login_path(locale) unless current_admin
+    end
+
+    def current_admin
+      @current_admin ||= admins_repository.find(id: session[:admin_id]) if session[:admin_id]
+    end
+
+    def locale
+      params[:locale]
+    end
+
+    def after_login_path(new_locale = locale)
+      admins_path(new_locale)
+    end
+
+    def after_logout_path(new_locale = locale)
+      root_path(new_locale)
+    end
+
+    private
+
+    def admins_repository
+      @admins_repository ||= Repository.new
+    end
+  end
+end
