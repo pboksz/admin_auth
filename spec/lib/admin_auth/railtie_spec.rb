f9a2ac752b66c40a5748476258f334f642091048
Phillip Boksz <phillip@lunarlogic.io>
2015-02-18 16:18:22 +0100

    Remove railtie spec

--- spec/lib/admin_auth/railtie_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe AdminAuth::Engine do
-  subject { AdminAuth::Railtie }
-
-  describe 'initializer added' do
-    it { expect(subject.initializers.map(&:name)).to include :admin_auth }
-  end
-end
