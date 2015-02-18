9253659ab3ba74cfb3a38511e97df945cfd22601
Phillip Boksz <phillip@lunarlogic.io>
2015-02-18 16:47:34 +0100

    Use initializer in railtie instead of config.after_initialize

--- lib/admin_auth/railtie.rb
+++ lib/admin_auth/railtie.rb
@@ -1,6 +1,6 @@
 module AdminAuth
   class Railtie < ::Rails::Railtie
-    config.after_initialize do
+    initializer :admin_auth do
       ::ApplicationController.include(AdminAuth::Controller) rescue nil
       ::Admin.include(AdminAuth::Model) rescue nil
     end
--- lib/admin_auth/version.rb
+++ lib/admin_auth/version.rb
@@ -1,3 +1,3 @@
 module AdminAuth
-  VERSION = '0.2.4'
+  VERSION = '0.2.5'
 end
